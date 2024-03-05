9679c9079856d45480f88f22bcc493aa92364001
Radu Apsan <radu.apsan@gmail.com>
2024-03-05 13:57:19 +0100

    stationary redo + data preprocessing

--- vr_runner.py
+++ vr_runner.py
@@ -45,7 +45,7 @@ def main():
     for app in config["apps"]:
         TRACE_PATH = os.path.join(current_dir, config['experiment_name'], config["device"], app["trace_path"])
         out = os.path.join(OUTPUT_DIR, app["name"], app["variation"])
-        out = os.path.join(out, "stationary_boundary_edge" +  TRACE_PATH.split("record")[1][0] + ".")
+        out = os.path.join(out, "stationary_boundary_redo" +  TRACE_PATH.split("record")[1][0] + ".")
 
         for i in range(config["repetitions"]):
             command = RUN_BENCH + ' -Mode "replay" ' + \
